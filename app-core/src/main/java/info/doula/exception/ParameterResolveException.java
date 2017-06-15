6f2e1092aa17d4060a20cfae4a7212be31d8cb1f
Hossain Doula <hossain.doula@itconquest.com>
2017-06-15 12:17:15 +0600

    Changes made
    - ParameterResolverException.java reverted back.

--- app-core/src/main/java/info/doula/exception/ParameterResolveException.java
+++ app-core/src/main/java/info/doula/exception/ParameterResolveException.java
@@ -1,21 +1,14 @@
 package info.doula.exception;
 
-import org.springframework.http.HttpStatus;
-
 /**
  * ParameterResolveException
  * @author hossaindoula<hossaindoula@gmail.com>
  */
-public class ParameterResolveException extends ApiException {
+public class ParameterResolveException extends Exception {
 
 	private static final long serialVersionUID = 7729045683866048832L;
 
-	ParameterResolveException(String message) {
+	public ParameterResolveException(String message) {
 		super(message);
-	}
-
-	@Override
-	HttpStatus getServiceStatus() {
-		return null;
 	}
 }
