3a7df8d337e8b69c706af8dae289e77672d3ca1b
Hossain Doula <hossain.doula@itconquest.com>
2017-08-17 18:11:38 +0600

    app-domain changed
    - @Entity annotation added.
    -

--- app-domain/src/main/java/info/doula/security/User.java
+++ app-domain/src/main/java/info/doula/security/User.java
@@ -2,11 +2,14 @@ package info.doula.security;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+
 /**
  * Created by doula_itc on 2017-08-16.
  */
 
 @Data
+@Entity
 public class User {
 
     private Integer id;
